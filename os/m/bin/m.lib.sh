f5aacf90f23efd06dbbb271d2ea6e2374bc8568a
massimilianonardi <massimiliano.nardi@gmail.com>
2024-06-19 09:41:49 +0200

    stable - before deep changes

--- os/m/bin/m.lib.sh
+++ os/m/bin/m.lib.sh
@@ -8,6 +8,15 @@ export MASSIMILIANO_NARDI="MASSIMILIANO_NARDI"
 
 #-------------------------------------------------------------------------------
 
+m_this()
+{
+  THIS_DIR="$(cd -P -- "${0%/*}"; pwd)"
+  THIS_NAME="${0##*/}"
+  THIS_PATH="$THIS_DIR/$THIS_NAME"
+}
+
+#-------------------------------------------------------------------------------
+
 m_this_fast()
 {
 #  THIS_PATH="$0"
@@ -41,7 +50,7 @@ m_start()
 {
   . m-lang.lib.sh
   . m-log.lib.sh
-  
+
   log_info "********************************************************************************"
   log_info "[START] $THIS_NAME" "$@"
   log_debug "[THIS_DIR] $THIS_DIR [PWD] $PWD"
@@ -55,15 +64,15 @@ m_end()
   . m-array.lib.sh
 #  . m-loop.lib.sh
   . m-parse.lib.sh
-  
+
   trace exec return args_parse "$@"
 #  trace call return args_parse "$@"
   EXIT_CODE="$?"
   [ "$EXIT_CODE" -eq "0" ] || log_fatal "EXIT_CODE: $EXIT_CODE"
-  
+
   log_info "[END $EXIT_CODE] $THIS_NAME" "$@"
   log_info "********************************************************************************"
-  
+
   exit "$EXIT_CODE"
 }
 
@@ -72,7 +81,7 @@ m_end()
 m_include()
 {
   INCLUDE_DIR="$THIS_DIR/include"
-  
+
   if [ -d "$INCLUDE_DIR" ]
   then
     log_debug "INCLUDE_DIR: $INCLUDE_DIR"
@@ -83,7 +92,7 @@ m_include()
       . "$k"
     done
   fi
-  
+
   if [ -d "$INCLUDE_DIR/$THIS_NAME" ]
   then
     log_debug "INCLUDE_DIR: $INCLUDE_DIR/$THIS_NAME"
@@ -112,22 +121,29 @@ m_conf_sys()
 {
   . m-sys.lib.sh
   . m-init.lib.sh
-  
+
   if [ "$M_CONTEXT" != "m_os" ]
   then
     log_fatal "this script need to be executed within an m-os context - M_CONTEXT: $M_CONTEXT"
     exit 1
   fi
-  
+
+  THIS_PACKAGE="${THIS_DIR%/bin}"
+  if [ -f "$THIS_PACKAGE/sys/name" ]
+  then
+    THIS_PACKAGE="$(cat "$THIS_PACKAGE/sys/name")"
+  else
+    THIS_PACKAGE="${THIS_PACKAGE##*/}"
+  fi
 #  work "$THIS_NAME"
   work "$THIS_PACKAGE"
-  
+
   if [ -f "$CONF_DIR/$THIS_PACKAGE.conf" ]
   then
     log_debug "CONF_FILE: $CONF_DIR/$THIS_PACKAGE.conf"
     . "$CONF_DIR/$THIS_PACKAGE.conf"
   fi
-  
+
   if [ -f "$CONF_DIR/$THIS_NAME.conf" ]
   then
     log_debug "CONF_FILE: $CONF_DIR/$THIS_NAME.conf"
@@ -139,10 +155,11 @@ m_conf_sys()
 
 m_command()
 {
-  m_this_fast
-  
+  # m_this_fast
+  m_this
+
   m_start "$@"
-  
+
   m_end "$@"
 }
 
@@ -150,18 +167,19 @@ m_command()
 
 m_script()
 {
-  m_this_resolved
-  
+  # m_this_resolved
+  m_this
+
   m_start "$@"
-  
+
   m_include
-  
+
   m_conf_local
   m_conf_sys
-  
+
   . m-filesystem.lib.sh
   . m-var.lib.sh
-  
+
   m_end "$@"
 }
 
