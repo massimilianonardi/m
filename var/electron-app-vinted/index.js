549149149281b93d3305cbfd93c53cf8f6f2c161
massimilianonardi <massimiliano.nardi@gmail.com>
2024-09-24 15:43:08 +0200

    electron app vinted

--- var/electron-app-vinted/index.js
+++ var/electron-app-vinted/index.js
@@ -3,39 +3,85 @@ var electron = require("electron");
 var {app, BrowserWindow, Menu, MenuItem, Tray, getCurrentWindow, globalShortcut} = require("electron");
 var {exec} = require("child_process");
 var https = require("https");
+var fs = require("fs");
+var path = require("path");
+// var url = require("url");
 
 var win = null;
+var dataPath = app.getAppPath() + "/data"
+
+function parseFavDump()
+{
+  var dumppath = dataPath + "/fav_dump";
+
+  var lastIndex = 99;
+  for(var i = 0; i < lastIndex; i++)
+  {
+    var fn = dumppath + "/favourites_" + i + ".json";
+    if(fs.existsSync(fn))
+    {
+      var text = fs.readFileSync(fn);
+      var json = JSON.parse(text);
+      console.log(fn);
+      var items = json.items;
+      for(var j = 0; j < items.length; j++)
+      {
+        var item = items[j];
+        var itemfn = dataPath + item.path;
+        fs.writeFileSync(itemfn, JSON.stringify(item), "utf-8");
+      }
+    }
+  }
+}
+
+var processFavChunk = function(url, i, lastChunk)
+{
+  var dlpath = dataPath + "/tmp";
+  https.get(url,(res) =>
+  {
+    var body = "";
+    res.on("data", (chunk) => {body += chunk;} );
+    res.on("end", () =>
+    {
+      try
+      {
+        var fn = dlpath + "/fav_" + i;
+        console.log(body);
+        // todo save to file
+        try
+        {
+          fs.writeFileSync(fn, body, "utf-8");
+        }
+        catch(e)
+        {
+          alert("Failed to save file");
+        }
+        var json = JSON.parse(body);
+        // do something with JSON
+        console.log(json);
+      }
+      catch (error)
+      {
+        console.error(error.message);
+      };
+    });
+  }).on("error", (error) =>
+  {
+    console.error(error.message);
+  });
+
+  // win.webContents.openDevTools();
+};
 
 var loadFavList = function()
 {
-  // win.loadURL("https://www.vinted.it");
-  // https://www.reddit.com/r/popular.json
-  let url = "https://www.reddit.com/r/popular.json";
-
-https.get(url,(res) => {
-    let body = "";
-
-    res.on("data", (chunk) => {
-        body += chunk;
-    });
-
-    res.on("end", () => {
-        try {
-            console.log(body);
-            let json = JSON.parse(body);
-            // do something with JSON
-            console.log(json);
-        } catch (error) {
-            console.error(error.message);
-        };
-    });
-
-}).on("error", (error) => {
-    console.error(error.message);
-});
-
-win.webContents.openDevTools();
-
+  var lastIndex = 35;
+  for(var i = 0; i < lastIndex; i++)
+  {
+    // https://www.vinted.it/api/v2/users/155761817/items/favourites?page=0&include_sold=true&per_page=90
+    var url = "https://www.vinted.it/api/v2/users/155761817/items/favourites?page=" + i + "&include_sold=true&per_page=20";
+    processFavChunk(url, i, i === lastIndex);
+  }
 };
 
 app.on("ready", function()
@@ -45,8 +91,9 @@ app.on("ready", function()
   var h = parseInt(height);
 
   win = new BrowserWindow({width: w, height: h});
-  // win.loadURL("https://www.vinted.it");
-  win.loadURL("https://www.vinted.it/member/items/favourite_list");
+  // win.loadURL("https://mail.google.com");
+  win.loadURL("https://www.vinted.it");
+  // win.loadURL("https://www.vinted.it/member/items/favourite_list");
   win.on("closed", () =>
   {
     win = null;
@@ -57,7 +104,6 @@ app.on("ready", function()
 //    getCurrentWindow().reload();
     win.reload();
   }
-//https://www.vinted.it/api/v2/users/155761817/items/favourites?page=2&include_sold=true&per_page=20
   globalShortcut.register("F5", reload);
   globalShortcut.register("CommandOrControl+R", reload);
 
@@ -71,7 +117,8 @@ app.on("ready", function()
 
   var actionsMenu = Menu.buildFromTemplate(
   [
-    {label: "Load Favourite List", type: "normal", click: loadFavList}
+    {label: "Load Favourite List", type: "normal", click: loadFavList},
+    {label: "Parse Favourite List", type: "normal", click: parseFavDump}
   ]);
 
   var actionsMenuItem = new MenuItem(
