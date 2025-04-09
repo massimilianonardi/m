1f83f177f556834d01d73d3d0177b00f784fd686
massimilianonardi <massimiliano.nardi@gmail.com>
2025-04-09 22:13:43 +0200

    app cmd

--- app/conf/litexl/.config/lite-xl/session.lua
+++ app/conf/litexl/.config/lite-xl/session.lua
@@ -1,1 +1,1 @@
-return {recents={}, window={[1]=1344,[2]=840,[3]=168,[4]=240,["n"]=4}, window_mode="normal", previous_find={[1]="THIS_DIR"}, previous_replace={[1]="SCRIPT_DIR"}}
+return {recents={}, window={[1]=1316,[2]=840,[3]=50,[4]=184,["n"]=4}, window_mode="normal", previous_find={[1]="THIS_DIR"}, previous_replace={[1]="SCRIPT_DIR"}}
