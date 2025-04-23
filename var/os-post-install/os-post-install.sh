dfcc2a410b8878bfa5c14d79a1f4c720d00f7580
massimilianonardi <massimiliano.nardi@gmail.com>
2025-04-23 08:05:39 +0200

    os post install

--- var/os-post-install/os-post-install.sh
+++ var/os-post-install/os-post-install.sh
@@ -14,6 +14,7 @@ sudo apt install -y curl
 sudo apt install -y meld
 sudo apt install -y openjdk-21-jdk
 sudo apt install -y remmina remmina-plugin-vnc
+sudo apt install -y libreoffice
 sudo dpkg -i "${0%/*}/../omnissa/Omnissa-Horizon-Client-2412-8.14.0-12437214089.x64.deb"
 sudo dpkg -i "${0%/*}/../pulsar_1.127.2025033016_amd64.deb"
 
