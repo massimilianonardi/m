8e893eaa70f6ad0096e7e3cfb0e1540ca3092864
massimilianonardi <massimiliano.nardi@gmail.com>
2025-05-13 12:28:09 +0200

    rsudo-jobs, enc lib, rsudo path to rpr + env enc

--- cmd/lib/waituser.lib.sh
+++ cmd/lib/waituser.lib.sh
@@ -2,7 +2,7 @@
 
 waituser()
 {
-  EXIT_CODE="$?"
+  # EXIT_CODE="$?"
 
   # detect if launched from gui or active terminal
   PARENT_PROCESS="$(ps -o 'cmd=' -p $(ps -o 'ppid=' -p $$))"
@@ -10,8 +10,8 @@ waituser()
   if [ "${PARENT_PROCESS%sh}" = "$PARENT_PROCESS" ]
   then
     echo ""
-    echo "--------------------------------------------------------------------------------"
-    echo "EXIT_CODE=$EXIT_CODE"
+    # echo "--------------------------------------------------------------------------------"
+    # echo "EXIT_CODE=$EXIT_CODE"
     echo "--------------------------------------------------------------------------------"
     read -p "press ENTER to exit" EXIT_VAR
   fi
