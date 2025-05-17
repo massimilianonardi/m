7410fb74e3e41cac27f56f73412a2ffda817bd93
massimilianonardi <massimiliano.nardi@gmail.com>
2025-05-17 10:10:06 +0200

    menu custom key + return selected custom key

--- cmd/test-command/dev/menu.lib.sh
+++ cmd/test-command/dev/menu.lib.sh
@@ -3,31 +3,24 @@
 menuctl()
 {
   case "$1" in
-    # "esc") quit;;
-    # "esc")
-    #   quit
-    # ;;
     "keys")
-      # if [ -z "$MENU_KEY_SEPARATOR" ]
-      # then
-      #   export MENU_KEY_SEPARATOR="="
-      # fi
       shift
-      key_var="$1"
-      shift
-      res_var="$1"
-      shift
-      set -- "$(
+
+      if [ -z "$MENU_KEY_SEPARATOR" ]
+      then
+        export MENU_KEY_SEPARATOR=":"
+      fi
+
+      set -- "$1" "$2" "$(
+        shift 2
         export MENU_CUSTOM_KEYS="$1"
         shift
         menu "$@"
-        # echo "$(menu "$@")"
-        # res="$(menu "$@")"
-        # echo "$res"
       )"
-      echo "arg1=$1"
-      eval "$key_var=\"${1%%${MENU_KEY_SEPARATOR}*}\""
-      eval "$res_var=\"${1#*${MENU_KEY_SEPARATOR}}\""
+
+      # echo "arg1=$1 arg2=$2 arg3=$3"
+      eval "$1=\"${3%%${MENU_KEY_SEPARATOR}*}\""
+      eval "$2=\"${3#*${MENU_KEY_SEPARATOR}}\""
     ;;
     *) exit 1;;
   esac
