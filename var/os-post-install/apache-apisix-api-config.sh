fa084e897b390319952053d3c2a6ac7460900375
massimilianonardi <massimiliano.nardi@gmail.com>
2025-11-05 15:53:05 +0100

    pkg geoserver

--- var/os-post-install/apache-apisix-api-config.sh
+++ var/os-post-install/apache-apisix-api-config.sh
@@ -118,3 +118,53 @@ curl -i "http://127.0.0.1:9180/apisix/admin/routes?api_key=$APISIX_KEY" -X PUT -
   },
   "service_id": "httpbin.org-https-service"
 }'
+
+
+
+#-------------------------------------------------------------------------------
+# auth test
+
+export APISIX_KEY="knpgKvNuusqgtxbsIhPuqGWEBWpDJwHg"
+export APISIX_HOST="dev.rpr-spa.it"
+export APISIX_PORT="9443"
+
+export KEYCLOAK_HOST="auth-keycloak.rpr-spa.it"
+export KEYCLOAK_PORT="8443"
+export KEYCLOAK_REALM="rpr"
+export KEYCLOAK_CLIENT_ID="apache-apisix-authz"
+export KEYCLOAK_CLIENT_SECRET="TkKZ5GXRqGPdYG6Ok8E9FtNHTo6aao3A"
+export KEYCLOAK_PROTECTED_URI="authz"
+
+export OIDC_CLIENT_ID="$KEYCLOAK_CLIENT_ID"
+export OIDC_CLIENT_SECRET="$KEYCLOAK_CLIENT_SECRET"
+
+curl -i "http://$KEYCLOAK_HOST:$KEYCLOAK_PORT/realms/$KEYCLOAK_REALM/protocol/openid-connect/token" -X POST \
+  -d 'grant_type=client_credentials' \
+  -d 'client_id='$KEYCLOAK_CLIENT_ID'' \
+  -d 'client_secret='$KEYCLOAK_CLIENT_SECRET''
+
+export ACCESS_TOKEN="eyJhbGciOiJIUzUxMiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICI3MDlhOTViMy1mNjA3LTRiN2YtOTYyOC1iOGI0MDhmZGMxZDEifQ.eyJleHAiOjAsImlhdCI6MTc2MjM1MTMwNiwianRpIjoiM2MzNzY3YTQtMTFjMS0wZDZjLTc3ZDYtZjFlNThiNzliNGEzIiwiaXNzIjoiaHR0cHM6Ly9hdXRoLWtleWNsb2FrLnJwci1zcGEuaXQ6ODQ0My9yZWFsbXMvcnByIiwiYXVkIjoiaHR0cHM6Ly9hdXRoLWtleWNsb2FrLnJwci1zcGEuaXQ6ODQ0My9yZWFsbXMvcnByIiwidHlwIjoiUmVnaXN0cmF0aW9uQWNjZXNzVG9rZW4iLCJyZWdpc3RyYXRpb25fYXV0aCI6ImF1dGhlbnRpY2F0ZWQifQ.Mm1DIQECpKR8Hpq5bRZ9rMF1HZ6sMU0yHkn7E6B-NLJMLxyrR2SNIE_CgEshVKxVhmmPLSKKwyDW7eM4M9GiiQ"
+
+curl "http://127.0.0.1:9180/apisix/admin/routes" -X PUT \
+  -H "X-API-KEY: ${APISIX_KEY}" \
+  -d '{
+    "id": "authz-keycloak-route",
+    "uri": "/anything",
+    "plugins": {
+      "authz-keycloak": {
+        "lazy_load_paths": true,
+        "resource_registration_endpoint": "https://'"$KEYCLOAK_HOST"':'"${KEYCLOAK_PORT}"'/realms/'"${KEYCLOAK_REALM}"'/authz/protection/resource_set",
+        "discovery": "https://'"${KEYCLOAK_HOST}"':'"${KEYCLOAK_PORT}"'/realms/'"${KEYCLOAK_REALM}"'/.well-known/uma2-configuration",
+        "client_id": "'"$KEYCLOAK_CLIENT_ID"'",
+        "client_secret": "'"$KEYCLOAK_CLIENT_SECRET"'"
+      }
+    },
+    "upstream": {
+      "type": "roundrobin",
+      "nodes": {
+        "httpbin.org": 1
+      }
+    }
+  }'
+
+curl "http://127.0.0.1:9080/anything" -H "Authorization: Bearer $ACCESS_TOKEN"
