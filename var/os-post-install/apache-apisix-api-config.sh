b9910a4ed3bad370a34d2c4db8c0d3e288acc874
massimilianonardi <massimiliano.nardi@gmail.com>
2026-02-05 13:38:16 +0100

    pkg opa

--- var/os-post-install/apache-apisix-api-config.sh
+++ var/os-post-install/apache-apisix-api-config.sh
@@ -9,7 +9,9 @@ export KEYCLOAK_PORT="8443"
 export KEYCLOAK_REALM="rpr"
 export KEYCLOAK_CLIENT_ID="apache-apisix"
 export KEYCLOAK_CLIENT_SECRET="WiLDWAtS9FMMjSzAP7fElIzvHn9ftrTU"
-export KEYCLOAK_PROTECTED_URI="auth"
+export KEYCLOAK_PUBLIC_URI="pub"
+export KEYCLOAK_AUTHENTICATED_URI="authn"
+export KEYCLOAK_AUTHORIZATED_URI="authz"
 
 
 
@@ -73,11 +75,118 @@ curl -i "http://127.0.0.1:9180/apisix/admin/services?api_key=$APISIX_KEY" -X PUT
 
 
 
+# plugins
+curl -i "http://127.0.0.1:9180/apisix/admin/plugin_configs?api_key=$APISIX_KEY" -X PUT -d '
+{
+  "id": "rewrite-plugins",
+  "plugins":
+  {
+    "proxy-rewrite":
+    {
+      "regex_uri": ["^/(.*)/(.*)", "/$2"]
+    }
+  }
+}'
+
+curl -i "http://127.0.0.1:9180/apisix/admin/plugin_configs?api_key=$APISIX_KEY" -X PUT -d '
+{
+  "id": "rewrite-authn-plugins",
+  "plugins":
+  {
+    "proxy-rewrite":
+    {
+      "regex_uri": ["^/(.*)/(.*)", "/$2"]
+    },
+    "openid-connect":
+    {
+      "bearer_only": false,
+      "session":
+      {
+        "secret": "change_to_whatever_secret_you_want"
+      },
+      "use_pkce": true,
+      "client_id": "'"${KEYCLOAK_CLIENT_ID}"'",
+      "client_secret": "'"${KEYCLOAK_CLIENT_SECRET}"'",
+      "discovery": "https://'"${KEYCLOAK_HOST}"':'"${KEYCLOAK_PORT}"'/realms/'"${KEYCLOAK_REALM}"'/.well-known/openid-configuration",
+      "scope": "openid profile",
+      "redirect_uri": "https://'"${APISIX_HOST}"':'"${APISIX_PORT}"'/'"${KEYCLOAK_AUTHENTICATED_URI}"'/callback"
+    }
+  }
+}'
+
+curl -i "http://127.0.0.1:9180/apisix/admin/plugin_configs?api_key=$APISIX_KEY" -X PUT -d '
+{
+  "id": "rewrite-authn-authz-plugins",
+  "plugins":
+  {
+    "proxy-rewrite":
+    {
+      "regex_uri": ["^/(.*)/(.*)", "/$2"]
+    },
+    "openid-connect":
+    {
+      "bearer_only": false,
+      "session":
+      {
+        "secret": "change_to_whatever_secret_you_want"
+      },
+      "use_pkce": true,
+      "client_id": "'"${KEYCLOAK_CLIENT_ID}"'",
+      "client_secret": "'"${KEYCLOAK_CLIENT_SECRET}"'",
+      "discovery": "https://'"${KEYCLOAK_HOST}"':'"${KEYCLOAK_PORT}"'/realms/'"${KEYCLOAK_REALM}"'/.well-known/openid-configuration",
+      "scope": "openid profile",
+      "redirect_uri": "https://'"${APISIX_HOST}"':'"${APISIX_PORT}"'/'"${KEYCLOAK_AUTHENTICATED_URI}"'/callback"
+    },
+    "opa":
+    {
+      "host": "http://dev.rpr-spa.it:8181",
+      "policy": "example1"
+    }
+  }
+}'
+
+
+
+# routes
+curl -i "http://127.0.0.1:9180/apisix/admin/routes?api_key=$APISIX_KEY" -X PUT -d '
+{
+  "id": "route-pub",
+  "uri": "/'"${KEYCLOAK_PUBLIC_URI}"'/*",
+  "plugin_config_id": "rewrite-plugins",
+  "service_id": "httpbin.org-https-service"
+}'
+
+
+
+curl -i "http://127.0.0.1:9180/apisix/admin/routes?api_key=$APISIX_KEY" -X PUT -d '
+{
+  "id": "route-authn",
+  "uri": "/'"${KEYCLOAK_AUTHENTICATED_URI}"'/*",
+  "plugin_config_id": "rewrite-authn-plugins",
+  "service_id": "httpbin.org-https-service"
+}'
+
+
+
+curl -i "http://127.0.0.1:9180/apisix/admin/routes?api_key=$APISIX_KEY" -X PUT -d '
+{
+  "id": "route-authz",
+  "uri": "/'"${KEYCLOAK_AUTHORIZATED_URI}"'/*",
+  "plugin_config_id": "rewrite-authn-authz-plugins",
+  "service_id": "httpbin.org-https-service"
+}'
+
+
+
+#-------------------------------------------------------------------------------
+
+
+
 # route auth authz-keycloak keycloak
 curl -i "http://127.0.0.1:9180/apisix/admin/routes?api_key=$APISIX_KEY" -X PUT -d '
 {
   "id": "route-auth",
-  "uri":"/'"${KEYCLOAK_PROTECTED_URI}"'/*",
+  "uri": "/'"${KEYCLOAK_AUTHENTICATED_URI}"'/*",
   "plugins":
   {
     "authz-keycloak":
@@ -105,7 +214,7 @@ curl -i "http://127.0.0.1:9180/apisix/admin/routes?api_key=$APISIX_KEY" -X PUT -
 curl -i "http://127.0.0.1:9180/apisix/admin/routes?api_key=$APISIX_KEY" -X PUT -d '
 {
   "id": "route-auth",
-  "uri":"/'"${KEYCLOAK_PROTECTED_URI}"'/*",
+  "uri": "/'"${KEYCLOAK_AUTHENTICATED_URI}"'/*",
   "plugins":
   {
     "openid-connect":
@@ -120,7 +229,7 @@ curl -i "http://127.0.0.1:9180/apisix/admin/routes?api_key=$APISIX_KEY" -X PUT -
       "client_secret": "'"${KEYCLOAK_CLIENT_SECRET}"'",
       "discovery": "https://'"${KEYCLOAK_HOST}"':'"${KEYCLOAK_PORT}"'/realms/'"${KEYCLOAK_REALM}"'/.well-known/openid-configuration",
       "scope": "openid profile",
-      "redirect_uri": "https://'"${APISIX_HOST}"':'"${APISIX_PORT}"'/'"${KEYCLOAK_PROTECTED_URI}"'/callback"
+      "redirect_uri": "https://'"${APISIX_HOST}"':'"${APISIX_PORT}"'/'"${KEYCLOAK_AUTHENTICATED_URI}"'/callback"
     },
     "authz-keycloak":
     {
@@ -150,98 +259,4 @@ curl http://127.0.0.1:9080/auth/ip -H "Authorization: Bearer ${ACCESS_TOKEN}"
 
 
 
-# route auth openid-connect keycloak
-curl -i "http://127.0.0.1:9180/apisix/admin/routes?api_key=$APISIX_KEY" -X PUT -d '
-{
-  "id": "route-auth",
-  "uri":"/'"${KEYCLOAK_PROTECTED_URI}"'/*",
-  "plugins":
-  {
-    "openid-connect":
-    {
-      "bearer_only": false,
-      "session":
-      {
-        "secret": "change_to_whatever_secret_you_want"
-      },
-      "use_pkce": true,
-      "client_id": "'"${KEYCLOAK_CLIENT_ID}"'",
-      "client_secret": "'"${KEYCLOAK_CLIENT_SECRET}"'",
-      "discovery": "https://'"${KEYCLOAK_HOST}"':'"${KEYCLOAK_PORT}"'/realms/'"${KEYCLOAK_REALM}"'/.well-known/openid-configuration",
-      "scope": "openid profile",
-      "redirect_uri": "https://'"${APISIX_HOST}"':'"${APISIX_PORT}"'/'"${KEYCLOAK_PROTECTED_URI}"'/callback"
-    },
-    "proxy-rewrite":
-    {
-      "regex_uri": ["^/(.*)/(.*)", "/$2"]
-    }
-  },
-  "service_id": "httpbin.org-https-service"
-}'
-
-
-
-# route pub
-curl -i "http://127.0.0.1:9180/apisix/admin/routes?api_key=$APISIX_KEY" -X PUT -d '
-{
-  "id": "route-pub",
-  "uri":"/pub/*",
-  "plugins":
-  {
-    "proxy-rewrite":
-    {
-      "regex_uri": ["^/(.*)/(.*)", "/$2"]
-    }
-  },
-  "service_id": "httpbin.org-https-service"
-}'
-
-
-
 #-------------------------------------------------------------------------------
-# auth test
-
-export APISIX_KEY="knpgKvNuusqgtxbsIhPuqGWEBWpDJwHg"
-export APISIX_HOST="dev.rpr-spa.it"
-export APISIX_PORT="9443"
-
-export KEYCLOAK_HOST="auth-keycloak.rpr-spa.it"
-export KEYCLOAK_PORT="8443"
-export KEYCLOAK_REALM="rpr"
-export KEYCLOAK_CLIENT_ID="apache-apisix-authz"
-export KEYCLOAK_CLIENT_SECRET="TkKZ5GXRqGPdYG6Ok8E9FtNHTo6aao3A"
-export KEYCLOAK_PROTECTED_URI="authz"
-
-export OIDC_CLIENT_ID="$KEYCLOAK_CLIENT_ID"
-export OIDC_CLIENT_SECRET="$KEYCLOAK_CLIENT_SECRET"
-
-curl -i "http://$KEYCLOAK_HOST:$KEYCLOAK_PORT/realms/$KEYCLOAK_REALM/protocol/openid-connect/token" -X POST \
-  -d 'grant_type=client_credentials' \
-  -d 'client_id='$KEYCLOAK_CLIENT_ID'' \
-  -d 'client_secret='$KEYCLOAK_CLIENT_SECRET''
-
-export ACCESS_TOKEN="eyJhbGciOiJIUzUxMiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICI3MDlhOTViMy1mNjA3LTRiN2YtOTYyOC1iOGI0MDhmZGMxZDEifQ.eyJleHAiOjAsImlhdCI6MTc2MjM1MTMwNiwianRpIjoiM2MzNzY3YTQtMTFjMS0wZDZjLTc3ZDYtZjFlNThiNzliNGEzIiwiaXNzIjoiaHR0cHM6Ly9hdXRoLWtleWNsb2FrLnJwci1zcGEuaXQ6ODQ0My9yZWFsbXMvcnByIiwiYXVkIjoiaHR0cHM6Ly9hdXRoLWtleWNsb2FrLnJwci1zcGEuaXQ6ODQ0My9yZWFsbXMvcnByIiwidHlwIjoiUmVnaXN0cmF0aW9uQWNjZXNzVG9rZW4iLCJyZWdpc3RyYXRpb25fYXV0aCI6ImF1dGhlbnRpY2F0ZWQifQ.Mm1DIQECpKR8Hpq5bRZ9rMF1HZ6sMU0yHkn7E6B-NLJMLxyrR2SNIE_CgEshVKxVhmmPLSKKwyDW7eM4M9GiiQ"
-
-curl "http://127.0.0.1:9180/apisix/admin/routes" -X PUT \
-  -H "X-API-KEY: ${APISIX_KEY}" \
-  -d '{
-    "id": "authz-keycloak-route",
-    "uri": "/anything",
-    "plugins": {
-      "authz-keycloak": {
-        "lazy_load_paths": true,
-        "resource_registration_endpoint": "https://'"$KEYCLOAK_HOST"':'"${KEYCLOAK_PORT}"'/realms/'"${KEYCLOAK_REALM}"'/authz/protection/resource_set",
-        "discovery": "https://'"${KEYCLOAK_HOST}"':'"${KEYCLOAK_PORT}"'/realms/'"${KEYCLOAK_REALM}"'/.well-known/uma2-configuration",
-        "client_id": "'"$KEYCLOAK_CLIENT_ID"'",
-        "client_secret": "'"$KEYCLOAK_CLIENT_SECRET"'"
-      }
-    },
-    "upstream": {
-      "type": "roundrobin",
-      "nodes": {
-        "httpbin.org": 1
-      }
-    }
-  }'
-
-curl "http://127.0.0.1:9080/anything" -H "Authorization: Bearer $ACCESS_TOKEN"
