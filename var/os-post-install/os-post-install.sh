488adfa82b58a28cd2a6b0f4f808ac3d8b1f75d7
massimilianonardi <massimiliano.nardi@gmail.com>
2026-03-19 10:15:28 +0100

    note

--- var/os-post-install/os-post-install.sh
+++ var/os-post-install/os-post-install.sh
@@ -11,13 +11,13 @@ sudo update-ca-certificates
 sudo apt install -y git-all
 sudo apt install -y screen
 sudo apt install -y curl
-sudo apt install -y podman
+#sudo apt install -y podman
 sudo apt install -y nmap
 sudo apt install -y meld
 sudo apt install -y openjdk-21-jdk
 sudo apt install -y remmina remmina-plugin-vnc
 sudo apt install -y libreoffice
-sudo dpkg -i "${0%/*}/../omnissa/Omnissa-Horizon-Client-2412-8.14.0-12437214089.x64.deb"
+#sudo dpkg -i "${0%/*}/../omnissa/Omnissa-Horizon-Client-2412-8.14.0-12437214089.x64.deb"
 
 sudo apt install -y nodejs npm
 sudo apt install -y yui-compressor
