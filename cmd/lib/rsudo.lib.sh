da1fc650cea77985c9cc025d14fce2808832a79d
massimilianonardi <massimiliano.nardi@gmail.com>
2026-04-09 11:19:55 +0200

    rsudo --shell

--- cmd/lib/rsudo.lib.sh
+++ cmd/lib/rsudo.lib.sh
@@ -14,12 +14,15 @@ rsudoenv()
 
 rsudoenv_load()
 {
-  encoded_file_import "$@"
+  log_echo "rsudoenv_load '$1'"
+  encoded_file_import "$1"
 }
 
 rsudoenv_save()
 {
   log_debug "$(echo "rsudoenv_save RSUDO_ENV:"; env_return export $(env_list "RSUDO_ENV"))"
+
+  log_echo "rsudoenv_save '$1'"
   echo "$(env_return export $(env_list "RSUDO_ENV"))" | encode > "$1"
 }
 
@@ -31,6 +34,7 @@ rsudoenv_editor()
 rsudoenv_edit()
 {
   (
+    export OPENSSL_PASS="$(readpass "enter decoding password for '$1':")"
     encoded_file_editor "$RSUDO_ENV_EDITOR"
     encoded_file_edit "$1"
   )
@@ -56,13 +60,29 @@ rsudoenv_set()
 {
   if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]
   then
-    log_debug "rsudoenv_set: empty name"
+    log_debug "rsudoenv_set: empty args"
     exit 1
   fi
 
   eval "export RSUDO_ENV_${1}_HOST=\"\${2}\""
   eval "export RSUDO_ENV_${1}_USER=\"\${3}\""
   eval "export RSUDO_ENV_${1}_PASS=\"\${4}\""
+
+  eval "log_debug \"rsudoenv_set: RSUDO_ENV_${1}_HOST=\$RSUDO_ENV_${1}_HOST | RSUDO_ENV_${1}_USER=\$RSUDO_ENV_${1}_USER\""
+  eval "log_trace \"rsudoenv_set: RSUDO_ENV_${1}_HOST=\$RSUDO_ENV_${1}_HOST | RSUDO_ENV_${1}_USER=\$RSUDO_ENV_${1}_USER | RSUDO_ENV_${1}_PASS=\$RSUDO_ENV_${1}_PASS\""
+}
+
+rsudoenv_unset()
+{
+  if [ -z "$1" ]
+  then
+    log_debug "rsudoenv_unset: empty name"
+    exit 1
+  fi
+
+  eval "unset RSUDO_ENV_${1}_HOST"
+  eval "unset RSUDO_ENV_${1}_USER"
+  eval "unset RSUDO_ENV_${1}_PASS"
 
   eval "log_debug \"rsudoenv_set: RSUDO_ENV_${1}_HOST=\$RSUDO_ENV_${1}_HOST | RSUDO_ENV_${1}_USER=\$RSUDO_ENV_${1}_USER\""
   eval "log_trace \"rsudoenv_set: RSUDO_ENV_${1}_HOST=\$RSUDO_ENV_${1}_HOST | RSUDO_ENV_${1}_USER=\$RSUDO_ENV_${1}_USER | RSUDO_ENV_${1}_PASS=\$RSUDO_ENV_${1}_PASS\""
