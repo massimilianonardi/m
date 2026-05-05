f479e5cd30038683f133eb0b27bdc88f36550dcc
massimilianonardi <massimiliano.nardi@gmail.com>
2026-05-05 15:37:49 +0200

    rsudo-env lib

--- cmd/rsudo/rsudo-env.lib.sh
+++ cmd/rsudo/rsudo-env.lib.sh
@@ -8,14 +8,12 @@
 
 rsudoenv()
 {
-  # "rsudoenv_${1}" "$@"
   rsudoenv_"$@"
 }
 
 # pass - asks user on tty for encoding/decoding password to keep in env for silent operations
 rsudoenv_pass()
 {
-  # export RSUDO_ENC_PASS="$(readpass "[rsudoenv] enter $([ -z "$1" ] && echo "DEFAULT" || echo "'$1'") encoding/decoding password")"
   RSUDO_ENC_PASS="$(readpass "[rsudoenv] enter $([ -z "$1" ] && echo "DEFAULT" || echo "'$1'") encoding/decoding password")"
 }
 
@@ -42,24 +40,15 @@ rsudoenv_load()
 # save $file - save each named group of connection variables in the form RSUDO_ENV_${name}_HOST, RSUDO_ENV_${name}_USER and RSUDO_ENV_${name}_PASS into encoded file $file (password from env)
 rsudoenv_save()
 {
-  # (
-  #   log_debug "$(echo "rsudoenv_save RSUDO_ENV:"; env_return export $(env_list "RSUDO_ENV"))"
-  #
-  #   log_echo "rsudoenv_save rsudo env connection vars to file:'$1'"
-  #   export ENC_PASS="$RSUDO_ENC_PASS"
-  #   env_return export $(env_list "RSUDO_ENV") | encode > "$1"
-  # )
   log_debug "$(echo "rsudoenv_save RSUDO_ENV:"; env_return export $(env_list "RSUDO_ENV"))"
 
   log_echo "rsudoenv_save rsudo env connection vars to file:'$1'"
-  # env_return export $(env_list "RSUDO_ENV") | ENC_PASS="$RSUDO_ENC_PASS" encode > "$1"
   env_return code $(env_list "RSUDO_ENV") | ENC_PASS="$RSUDO_ENC_PASS" encode > "$1"
 }
 
 # sets the editor to use when calling edit
 rsudoenv_editor()
 {
-  # export RSUDO_ENV_EDITOR="$1"
   RSUDO_ENV_EDITOR="$1"
 }
 
@@ -68,7 +57,6 @@ rsudoenv_edit()
 {
   (
     # silently reusing env pass for decoding/re-encoding
-    # export ENC_PASS="$RSUDO_ENC_PASS"
     ENC_PASS="$RSUDO_ENC_PASS"
     encoded_file_editor "$RSUDO_ENV_EDITOR"
     encoded_file_edit "$1"
@@ -94,8 +82,6 @@ rsudoenv_get()
   then
     # user@host | @host -> always asks for password from tty user input
     log_debug "rsudoenv_get: ARG=$1 - RSUDO_HOST=${1#*@} - RSUDO_USER=${1%@*}"
-    # export RSUDO_HOST="${1#*@}"
-    # export RSUDO_USER="${1%@*}"
     RSUDO_HOST="${1#*@}"
     RSUDO_USER="${1%@*}"
   else
@@ -108,9 +94,6 @@ rsudoenv_get()
 
     set -- "${1#*:}"
 
-    # eval "export RSUDO_HOST=\"\$RSUDO_ENV_${1}_HOST\""
-    # eval "export RSUDO_USER=\"\$RSUDO_ENV_${1}_USER\""
-    # eval "export RSUDO_PASSWORD=\"\$RSUDO_ENV_${1}_PASS\""
     eval "RSUDO_HOST=\"\$RSUDO_ENV_${1}_HOST\""
     eval "RSUDO_USER=\"\$RSUDO_ENV_${1}_USER\""
     eval "RSUDO_PASSWORD=\"\$RSUDO_ENV_${1}_PASS\""
@@ -126,7 +109,6 @@ rsudoenv_get()
   then
     # if user is not stored, it is intentionally wanted set it to current user
     log_info "rsudoenv_get: empty RSUDO_USER, setting it to '$USER'"
-    # export RSUDO_USER="$USER"
     RSUDO_USER="$USER"
   fi
 
@@ -134,7 +116,6 @@ rsudoenv_get()
   then
     # user@host | @host -> always asks for password from tty user input
     # if password is not stored, it is intentionally wanted set it to ask for it at runtime
-    # export RSUDO_PASSWORD="$(readpass "[rsudo] Enter password for ${RSUDO_USER}@${RSUDO_HOST}:")"
     RSUDO_PASSWORD="$(readpass "[rsudo] Enter password for ${RSUDO_USER}@${RSUDO_HOST}:")"
   fi
 
@@ -154,18 +135,12 @@ rsudoenv_set()
 
   if [ -z "$2" ]
   then
-    # eval "export RSUDO_ENV_${1}_HOST=\"\${RSUDO_HOST}\""
-    # eval "export RSUDO_ENV_${1}_USER=\"\${RSUDO_USER}\""
-    # eval "export RSUDO_ENV_${1}_PASS=\"\${RSUDO_PASSWORD}\""
     eval "RSUDO_ENV_${1}_HOST=\"\${RSUDO_HOST}\""
     eval "RSUDO_ENV_${1}_USER=\"\${RSUDO_USER}\""
     eval "RSUDO_ENV_${1}_PASS=\"\${RSUDO_PASSWORD}\""
   else
     eval "$(
       rsudoenv_get "$2"
-      # echo "export RSUDO_ENV_${1}_HOST=\"${RSUDO_HOST}\""
-      # echo "export RSUDO_ENV_${1}_USER=\"${RSUDO_USER}\""
-      # echo "export RSUDO_ENV_${1}_PASS=\"${RSUDO_PASSWORD}\""
       echo "RSUDO_ENV_${1}_HOST=\"${RSUDO_HOST}\""
       echo "RSUDO_ENV_${1}_USER=\"${RSUDO_USER}\""
       echo "RSUDO_ENV_${1}_PASS=\"${RSUDO_PASSWORD}\""
