9f0d7ea1c0dfa63967ab63ba3f95e8e288f98e8d
massimilianonardi <massimiliano.nardi@gmail.com>
2026-05-28 15:15:33 +0200

    env_set_from_template

--- cmd/rsudo/rsudo-mod-apisix.lib.sh
+++ cmd/rsudo/rsudo-mod-apisix.lib.sh
@@ -2,8 +2,6 @@
 
 . log.lib.sh
 
-APISIX_ADMIN_URL="http://127.0.0.1:9180/apisix/admin"
-
 KEYCLOAK_CLIENT_ID_AUTHN_AUTHZ="apisix-authn-authz"
 KEYCLOAK_CLIENT_SECRET="WiLDWAtS9FMMjSzAP7fElIzvHn9ftrTU"
 
@@ -12,32 +10,68 @@ export PROXY_HOST="apps.rpr-spa.it"
 export APISIX_HOST="apisix.rpr-spa.it"
 export APISIX_PORT="9443"
 
-export KEYCLOAK_HOST="keycloak.rpr-spa.it"
-export KEYCLOAK_PORT="8443"
-export KEYCLOAK_REALM="rpr"
+KEYCLOAK_HOST="keycloak.rpr-spa.it"
+KEYCLOAK_PORT="8443"
+KEYCLOAK_REALM="rpr"
 
-export KEYCLOAK_REALM_URL="https://${KEYCLOAK_HOST}:${KEYCLOAK_PORT}/realms/${KEYCLOAK_REALM}"
+KEYCLOAK_REALM_URL="https://${KEYCLOAK_HOST}:${KEYCLOAK_PORT}/realms/${KEYCLOAK_REALM}"
 
-export AUTHN_DISCOVERY_URL="${KEYCLOAK_REALM_URL}/.well-known/openid-configuration"
+AUTHN_DISCOVERY_URL="${KEYCLOAK_REALM_URL}/.well-known/openid-configuration"
 export AUTHZ_DISCOVERY_URL="${KEYCLOAK_REALM_URL}/.well-known/uma2-configuration"
 
 export AUTHN_REDIRECT_BASE_URL="https://${APISIX_HOST}:${APISIX_PORT}"
 #export AUTHN_REDIRECT_BASE_URL="https://${PROXY_HOST}/apisix"
 
-# url_host, url_command, key, url_param -> url_param will be splitted in more params by specific api
+APISIX_PLUGIN_CONF_KEYCLOAK_AUTHN_TEMPLATE='
+"openid-connect":
+{
+  "client_id": "${1}",
+  "client_secret": "${2}",
+  "discovery": "'"${AUTHN_DISCOVERY_URL}"'",
+  "redirect_uri": "'"${AUTHN_REDIRECT_BASE_URL}"'/${3}/callback",
+  "scope": "openid email profile",
+  "use_pkce": true,
+  "bearer_only": false,
+  "access_token_in_authorization_header": true,
+  "set_userinfo_header": true,
+  "session":
+  {
+    "secret": "apisix_openid_connect_session_secret"
+  }
+}
+'
 
 #------------------------------------------------------------------------------
 
-rsudo_mod_apisix_test()
+rsudo_mod_apisix_json_string_or_array()
 {
-  log_debug "inside apisix"
-  rsudo << 'EOF'
-ADMIN_KEY_PREFIX="$(cat "/usr/local/apisix/conf/config.yaml" | sed -n "s/admin_key:.*//p")"
-ADMIN_KEY="$(cat "/usr/local/apisix/conf/config.yaml" | sed "s/$ADMIN_KEY_PREFIX//" | sed -n '/admin_key:/,/^[[:lower:]]/{//!p;}' | tr '\n' '|' | sed -e 's/ //g' -e 's/.*admin|//' -e 's/|.*//' -e 's/key://')"
+(
+  if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]
+  then
+    exit 1
+  fi
 
-echo "ADMIN_KEY_PREFIX='$ADMIN_KEY_PREFIX'"
-echo "ADMIN_KEY='$ADMIN_KEY'"
-EOF
+  JSON_KEY="$1"
+  shift
+
+  JSON_KEY_ARRAY="$1"
+  shift
+
+  set -- $1
+  if [ "$#" = "1" ]
+  then
+    echo "\"$JSON_KEY\": \"$1\""
+  else
+    JSON_ARRAY="\"$1\""
+    shift
+    while [ "$#" -gt "0" ]
+    do
+      JSON_ARRAY="${JSON_ARRAY}, \"$1\""
+      shift
+    done
+    echo "\"$JSON_KEY_ARRAY\": [$JSON_ARRAY]"
+  fi
+)
 }
 
 #-------------------------------------------------------------------------------
@@ -64,17 +98,353 @@ rsudo_mod_apisix_keyset()
 
 #-------------------------------------------------------------------------------
 
-rsudo_mod_apisix_command()
+rsudo_mod_apisix_api()
 {
-  if [ -z "$1" ] || [ -z "$2" ]
+
+  if [ -z "$APISIX_KEY" ]
   then
-    return 1
+    APISIX_KEY="$(rsudo_mod_apisix_key)"
   fi
 
-  rsudo << EOF
-curl -i "${APISIX_ADMIN_URL}/${1}?api_key=${APISIX_KEY}" -X PUT -d '${2}'
-EOF
+(
+  if [ -z "$1" ]
+  then
+    exit 1
+  fi
 
+  if [ -z "$APISIX_ADMIN_URL" ]
+  then
+    APISIX_ADMIN_URL="http://127.0.0.1:9180/apisix/admin"
+  fi
+
+  if [ -z "$2" ]
+  then
+    HTTP_METHOD="GET"
+  else
+    HTTP_METHOD="$2"
+  fi
+
+  if [ -z "$3" ]
+  then
+    rsudo << EOF
+curl -i "${APISIX_ADMIN_URL}/${1}" -H "X-API-KEY: ${APISIX_KEY}" -X ${HTTP_METHOD}
+EOF
+  else
+    rsudo << EOF
+curl -i "${APISIX_ADMIN_URL}/${1}" -H "X-API-KEY: ${APISIX_KEY}" -X ${HTTP_METHOD} -d '${3}'
+EOF
+  fi
+)
+}
+
+#-------------------------------------------------------------------------------
+
+rsudo_mod_apisix_get()
+{
+  rsudo_mod_apisix_api "$1" "GET"
+}
+
+#-------------------------------------------------------------------------------
+
+rsudo_mod_apisix_put()
+{
+  rsudo_mod_apisix_api "$1" "PUT" "$2"
+}
+
+#-------------------------------------------------------------------------------
+
+rsudo_mod_apisix_post()
+{
+  rsudo_mod_apisix_api "$1" "POST" "PUT" "$2"
+}
+
+#-------------------------------------------------------------------------------
+
+rsudo_mod_apisix_del()
+{
+  rsudo_mod_apisix_api "$1" "DELETE"
+}
+
+#-------------------------------------------------------------------------------
+
+rsudo_mod_apisix_delete()
+{
+  rsudo_mod_apisix_delete_"$@"
+}
+
+#-------------------------------------------------------------------------------
+
+rsudo_mod_apisix_delete_service()
+{
+(
+  if [ -z "$1" ]
+  then
+    exit 1
+  fi
+
+  ID="$1"
+
+  rsudo_mod_apisix_del "services/${ID}"
+)
+}
+
+#-------------------------------------------------------------------------------
+
+rsudo_mod_apisix_delete_route()
+{
+(
+  if [ -z "$1" ]
+  then
+    exit 1
+  fi
+
+  ID="$1"
+
+  rsudo_mod_apisix_del "routes/${ID}"
+)
+}
+
+#-------------------------------------------------------------------------------
+
+rsudo_mod_apisix_delete_stream_route()
+{
+(
+  if [ -z "$1" ]
+  then
+    exit 1
+  fi
+
+  ID="$1"
+
+  rsudo_mod_apisix_del "stream_routes/${ID}"
+)
+}
+
+#-------------------------------------------------------------------------------
+
+rsudo_mod_apisix_delete_upstream()
+{
+(
+  if [ -z "$1" ]
+  then
+    exit 1
+  fi
+
+  ID="$1"
+
+  rsudo_mod_apisix_del "upstreams/${ID}"
+)
+}
+
+#-------------------------------------------------------------------------------
+
+rsudo_mod_apisix_delete_consumer()
+{
+(
+  if [ -z "$1" ]
+  then
+    exit 1
+  fi
+
+  ID="$1"
+
+  rsudo_mod_apisix_del "consumers/${ID}"
+)
+}
+
+#-------------------------------------------------------------------------------
+
+rsudo_mod_apisix_delete_consumer_group()
+{
+(
+  if [ -z "$1" ]
+  then
+    exit 1
+  fi
+
+  ID="$1"
+
+  rsudo_mod_apisix_del "consumer_groups/${ID}"
+)
+}
+
+#-------------------------------------------------------------------------------
+
+rsudo_mod_apisix_delete_ssl()
+{
+(
+  if [ -z "$1" ]
+  then
+    exit 1
+  fi
+
+  ID="$1"
+
+  rsudo_mod_apisix_del "ssls/${ID}"
+)
+}
+
+#-------------------------------------------------------------------------------
+
+rsudo_mod_apisix_delete_global_rule()
+{
+(
+  if [ -z "$1" ]
+  then
+    exit 1
+  fi
+
+  ID="$1"
+
+  rsudo_mod_apisix_del "global_rules/${ID}"
+)
+}
+
+#-------------------------------------------------------------------------------
+
+rsudo_mod_apisix_delete_plugin_metadata()
+{
+(
+  if [ -z "$1" ]
+  then
+    exit 1
+  fi
+
+  ID="$1"
+
+  rsudo_mod_apisix_del "plugin_metadata/${ID}"
+)
+}
+
+#-------------------------------------------------------------------------------
+
+rsudo_mod_apisix_delete_plugin_config()
+{
+(
+  if [ -z "$1" ]
+  then
+    exit 1
+  fi
+
+  ID="$1"
+
+  rsudo_mod_apisix_del "plugin_configs/${ID}"
+)
+}
+
+#-------------------------------------------------------------------------------
+
+rsudo_mod_apisix_delete_secret()
+{
+(
+  if [ -z "$1" ]
+  then
+    exit 1
+  fi
+
+  ID="$1"
+
+  rsudo_mod_apisix_del "secrets/${ID}"
+)
+}
+
+#-------------------------------------------------------------------------------
+
+rsudo_mod_apisix_delete_proto()
+{
+(
+  if [ -z "$1" ]
+  then
+    exit 1
+  fi
+
+  ID="$1"
+
+  rsudo_mod_apisix_del "protos/${ID}"
+)
+}
+
+#-------------------------------------------------------------------------------
+
+rsudo_mod_apisix_create()
+{
+  rsudo_mod_apisix_create_"$@"
+}
+
+#-------------------------------------------------------------------------------
+
+rsudo_mod_apisix_create_service()
+{
+(
+  if [ -z "$1" ] || [ -z "$2" ]
+  then
+    exit 1
+  fi
+
+  ID="$1"
+  PARAMS="$2"
+
+  rsudo_mod_apisix_put "services" '
+  {
+    "id": "'"${ID}"'",
+    "name": "'"${ID}"'",
+    '"${PARAMS}"'
+  }
+  '
+)
+}
+
+#-------------------------------------------------------------------------------
+
+rsudo_mod_apisix_create_service_simple()
+{
+(
+  if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]
+  then
+    exit 1
+  fi
+
+  ID="$1"
+  SCHEME="$2"
+  ADDRESS_PORT="$3"
+
+  rsudo_mod_apisix_create_service "${ID}" '
+  "upstream":
+  {
+    "scheme": "'"${SCHEME}"'",
+    "retries": 1,
+    "type": "roundrobin",
+    "nodes":
+    {
+      "'"${ADDRESS_PORT}"'": 1
+    }
+  }
+  '
+)
+}
+
+#-------------------------------------------------------------------------------
+
+rsudo_mod_apisix_create_route()
+{
+(
+  if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]
+  then
+    exit 1
+  fi
+
+  ID="$1"
+  URI_OR_URIS="$(rsudo_mod_apisix_json_string_or_array "uri" "uris" $2)"
+  PARAMS="$3"
+
+  rsudo_mod_apisix_put "routes" '
+  {
+    "id": "'"${ID}"'",
+    "name": "'"${ID}"'",
+    '"${URI_OR_URIS}"',
+    '"${PARAMS}"'
+  }'
+)
 }
 
 #-------------------------------------------------------------------------------
